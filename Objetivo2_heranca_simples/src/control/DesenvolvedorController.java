c2c4d7fb30ddbaaffb23ce46155c12b1de6d2934
Maisson Leal da Silva <maisson.ifsul@gmail.com>
2023-09-19 01:43:55 -0300

    mudanças de bonus e to String

--- Objetivo2_heranca_simples/src/control/DesenvolvedorController.java
+++ Objetivo2_heranca_simples/src/control/DesenvolvedorController.java
@@ -2,17 +2,29 @@ package control;
 // em produção
 import model.Desenvolvedor;
 import model.Funcionario;
+
+import java.text.NumberFormat;
+
 public class DesenvolvedorController {
     public static void main(String[] args) {
 
         Desenvolvedor dev1 = new Desenvolvedor();
         Desenvolvedor dev2 = new Desenvolvedor("Gohan", 5000);
 
-        System.out.println(dev2);
+        System.out.println(dev1);
 
-        dev2.setSalario(5008);
+        dev1.setSalario(5000);
+        System.out.println(dev1);
+        System.out.println();
+        System.out.println("Salário + Bonus "+ NumberFormat.getCurrencyInstance().format(dev1.getBonus()));
+
+        System.out.println("-------------------");
+
         System.out.println(dev2);
-        System.out.println(dev2.getBonus(2));
+        dev2.setSalario(6800);
+        System.out.println(dev2);
+        System.out.println();
+        System.out.println("Salário + Bonus  = "+ NumberFormat.getCurrencyInstance().format(dev1.getBonus()));
 
     }
 }
--- Objetivo2_heranca_simples/src/model/Desenvolvedor.java
+++ Objetivo2_heranca_simples/src/model/Desenvolvedor.java
@@ -4,7 +4,20 @@ public class Desenvolvedor extends Funcionario{
     public Desenvolvedor() {
     }
 
+    public double getBonus() {
+        double bonusDev = getSalario() + (getSalario() * 0.05);
+        return bonusDev;
+    }
+
     public Desenvolvedor(String nome, double salario) {
         super(nome, salario);
+    }
+
+    @Override
+    public String toString() {
+        return "\nDesenvolvedor{" +
+                "nome='" + getNome() + '\'' +
+                ", salario=" + getSalario() +
+                '}';
     }
 }
